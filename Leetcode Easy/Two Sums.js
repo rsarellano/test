885a139eb249ee8e42e79efda78966e89f9f8412
rsarellano <rjsarellano@gmail.com>
2025-03-18 04:39:54 +0800

    Two sum update

--- Leetcode Easy/Two Sums.js
+++ Leetcode Easy/Two Sums.js
@@ -11,14 +11,16 @@
 //     }
 //     return [[], target]
 
-    
 // }
 
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
 
-var twoSum = function(nums, target) {
-
-    
-
-}
-
-console.log(twoSum([2,7,11,15],9))
+console.log(twoSum([2, 7, 11, 15], 9));
